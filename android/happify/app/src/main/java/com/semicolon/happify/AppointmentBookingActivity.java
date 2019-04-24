34914e5c26b5c1ad810a2ff6b5f82a0e99e2f8c0
Raghuram Veeramallu <rockstar@dynamo.local>
2019-04-24 15:37:38 +0530

    small_fixes

--- android/happify/app/src/main/java/com/semicolon/happify/AppointmentBookingActivity.java
+++ android/happify/app/src/main/java/com/semicolon/happify/AppointmentBookingActivity.java
@@ -3,24 +3,38 @@ package com.semicolon.happify;
 import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class AppointmentBookingActivity extends Fragment {
 
     View myView;
-    Spinner cityDd, areaDd, doctorDd;
-
+    private Spinner cityDd, areaDd, doctorDd;
+    private ProgressDialog progressDialog;
+    private FirebaseFirestore db ;
+    HashMap<String, String> cityArea;
+    HashMap<String, String> areaDoc;
 
     @Nullable
     @Override
@@ -28,12 +42,62 @@ public class AppointmentBookingActivity extends Fragment {
         myView = inflater.inflate(R.layout.appointment_booking, container, false);
 
         cityDd = (Spinner) myView.findViewById(R.id.citySpinner);
+        areaDd = (Spinner) myView.findViewById(R.id.areaSpinner);
+        doctorDd = (Spinner) myView.findViewById(R.id.doctorSpinner);
 
-        String[] items = new String[]{"1", "2", "three"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item,items);
-        cityDd.setAdapter(adapter);
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Please wait...");
+        progressDialog.show();
+
+        setSpinners();
+
+        db = FirebaseFirestore.getInstance();
+
+        //Loaddatafromfirebase();
+        progressDialog.dismiss();
 
         return myView;
     }
+
+    public void setSpinners(){
+        String[] cities = new String[]{"Noida", "Delhi", "Mumbai"};
+        String[] areas = new String[]{"GB Nagar", "Dadri", "Sonpet"};
+        String[] doctors = new String[]{"SD", "VS", "RJ"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, cities);
+        cityDd.setAdapter(adapter);
+        ArrayAdapter<String> areaAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, areas);
+        areaDd.setAdapter(areaAdapter);
+        ArrayAdapter<String> doctorAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, doctors);
+        doctorDd.setAdapter(doctorAdapter);
+    }
+
+
+    void Loaddatafromfirebase(){
+
+        db.collection("PsychiatristLocations")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        cityArea = new HashMap<>();
+                        areaDoc = new HashMap<>();
+                        for (DocumentSnapshot document : task.getResult()) {
+                            String cityName = document.getString("City");
+                            String areaName = document.getString("Location");
+                            String doctorName = document.getString("Doctor");
+                            cityArea.put(cityName, areaName);
+                            areaDoc.put(areaName, doctorName);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error getting documents.");
+                    }
+                });
+
+    }
+
 
 }
